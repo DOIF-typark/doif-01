d7f925cef836c8e906eac2a38ea243a3babbca5d
doif.typark <doif.typark@gmail.com>
2020-07-28 18:17:22 +0900

    4일차 useEffect, useMemo, useCallback, React.memo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useMemo, useCallback } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Hello from './components/Hello';
@@ -7,36 +7,46 @@ import Wrapper from './components/Wrapper';
 import Input from './components/Input';
 import UserList from './components/UserList';
 
+function getActiveUser(users) {
+  console.log('카운팅 중...');
+  return users.filter(user => user.active).length;
+}
+
 function App() {
   // const [value, setValue] = useState(); // React의 hook
   const nextId = useRef(5);
   const [inputs, setInputs] = useState({
     name:'',
-    email:''
+    email:'',
+    active: false
   });
 
   const [users, setUsers] = useState([
     {
         id: '1',
         name:'mj',
-        email:'mj@naver.com'
+        email:'mj@naver.com',
+        active: false
     },
     {
         id: '2',
         name:'ty',
-        email:'ty@naver.com'
+        email:'ty@naver.com',
+        active: false
     },
     {
         id: '3',
         name:'hy',
-        email:'hy@naver.com'
+        email:'hy@naver.com',
+        active: false
     },
     {
         id: '4',
         name:'js',
-        email:'js@naver.com'
+        email:'js@naver.com',
+        active: false
     }
-])
+  ])
 
   const onInputChange = (e) => {
     const {name, value} = e.target;
@@ -63,10 +73,26 @@ function App() {
     })
     nextId.current += 1;
   }
+
+  const onToggle = useCallback((id) => {
+    setUsers((users) =>
+      users.map(user => (
+        user.id === id ? {...user, active: !user.active} : user
+      ))
+    );
+  }, []);
+
+  const onRemove = useCallback((id) => {
+    setUsers((users) => users.filter(user => id!==user.id));
+  }, []);
+
+  const count = useMemo(() => getActiveUser(users), [users])
+
   return (
     <>
       <Input inputs={inputs} onInputChange={onInputChange} onAdd={onAdd}/>
-      <UserList users={users}/>
+      <UserList users={users} onToggle={onToggle} onRemove={onRemove}/>
+      <div>활성화된 유저수: {count}</div>
     </>
   );
 }
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,13 +1,38 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 
-function UserList({users}) {
+const User = React.memo(({user, onToggle, onRemove}) => {
+    console.log('User 리렌더링');
+    useEffect(() => {
+        console.log(`${user.name}이 마운트됨`);
+
+        return () => {
+            console.log(`${user.name}이 언마운트됨`);
+        }
+    }, [user])
+
+    const style = {
+        color: user.active ? 'green' : 'black',
+        cursor: 'pointer'
+    };
+    return (
+        <>
+        <li style={style} onClick={() => onToggle(user.id)}>
+            이름: {user.name} 이메일: {user.email}
+        </li>
+        <button onClick={() => onRemove(user.id)}>삭제</button>
+        </>
+    );
+});
+
+function UserList({users, onToggle, onRemove}) {
     
   return (
-  <ul>
-      {users.map((user, index) => (
-      <li key={user.id}>이름: {user.name} 이메일: {user.email}</li>
-      ))}
-  </ul>);
+    <ul>
+        {users.map((user) => (
+        <User key={user.id} user={user} onToggle={onToggle} onRemove={onRemove}/>
+        ))}
+    </ul>
+  );
 }
 
-export default UserList;
+export default React.memo(UserList);
